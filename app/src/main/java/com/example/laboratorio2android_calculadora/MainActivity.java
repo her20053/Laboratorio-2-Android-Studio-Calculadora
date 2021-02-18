0e6f311c4eb33ee9b87edaf37abba6bcb5045268
Jose Andres Hernandez Guerra <her20053@uvg.edu.gt>
2021-02-18 10:29:43 -0600

    Agregados botones
    Agregados los botones y colores extra

--- app/src/main/java/com/example/laboratorio2android_calculadora/MainActivity.java
+++ app/src/main/java/com/example/laboratorio2android_calculadora/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.laboratorio2android_calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void limpiarEnClick(View view) {
+    }
+
+    public void parentesisEnClick(View view) {
+    }
+
+    public void igualEnClick(View view) {
+    }
+
+    public void ceroEnClick(View view) {
+    }
+
+    public void puntoEnClick(View view) {
+    }
+
+    public void tresEnClick(View view) {
+    }
+
+    public void dosEnClick(View view) {
+    }
+
+    public void unoEnClick(View view) {
+    }
+
+    public void menosEnClick(View view) {
+    }
+
+    public void seisEnClick(View view) {
+    }
+
+    public void cincoEnClick(View view) {
+    }
+
+    public void cuatroEnClick(View view) {
+    }
+
+    public void multiplicarEnClick(View view) {
+    }
+
+    public void nueveEnClick(View view) {
+    }
+
+    public void ochoEnClick(View view) {
+    }
+
+    public void sieteEnClick(View view) {
+    }
+
+    public void divisionEnClick(View view) {
+    }
+
+    public void potenciaEnClick(View view) {
     }
 }
